94a4a34c657b57dc12ced8c11c79847deb14ee41
Kegan Holtzhausen <marzubus@gmail.com>
2015-06-07 23:52:17 +0200

    clear script for nuking history

---
+++ clear.sh
@@ -0,0 +1,3 @@
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch conf.json' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch ./src/main/resources/conf.json' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch ./src/test/resources/conf.json' --prune-empty --tag-name-filter cat -- --all
