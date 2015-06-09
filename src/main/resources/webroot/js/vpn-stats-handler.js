c5b8c5b970144d1fd7374831d988f2edd13da990
Kegan Holtzhausen <Kegan.Holtzhausen@unibet.com>
2015-06-09 19:53:28 +0200

    tweaks and cleanup

--- src/main/resources/webroot/js/vpn-stats-handler.js
+++ src/main/resources/webroot/js/vpn-stats-handler.js
@@ -1,16 +1,4 @@
-    /*
 
-    Stats Handler, this take in a message and processes the message. 
-
-    A Message MUST contain
-
-    topic: the name of the Metric / VPN / ...
-    data: JSON version of ALL data from Solace RPC
-    config: information on how to interpret data
-
-    TODO FIXME refactor to be the ONLY data processor
-
-    */
 
 
     // a place to store VPN objects
@@ -55,7 +43,35 @@
     }
 
 
-    // the main handler, register this as the callback in the eventBus registerHandler code of the evenbus client.
+
+    /*
+
+    VPN STATS HANDLER
+    
+    This Stats Handler processes a Message which contains "topic", "data", "confif", the data MUST be a  JsonObject ( NOT Array )
+    the data_path of the object MUST be the where the plotable key:values are, though some values can be in sub leafs which the 
+    config can hint at with more data_paths.
+
+    example message:
+
+    {  
+       "topic":"metricName / VPN name",
+       "data":{  
+          // entire RPC response from Solace in JSON format
+       },
+       "config":{
+          // the data_path tells the stats handler where to find the keys and values
+          "data_path":"rpc-reply.rpc.show.message-vpn.vpn.stats",
+          "view":"generic_vpn_stats",
+          "view_format": {
+             // view document describes what to plot and when to descend into sub-keys
+          }
+       }
+    }
+    
+
+    */
+
     var vpn_stats_handler = function(msg) {
         
         // on chance of logging, set to true
@@ -71,14 +87,14 @@
         var data = data_shifter(msg.data, msg.config['data_path'], logging );
 
         // read the config out of the message
-        var config = msg.config['view_format'];
+        var view_format = msg.config['view_format'];
         
         if (logging) {
-            console.log("vpn_data config");
-            console.log(msg.config);
+            console.log("vpn_stats_handler config:");
+            console.log(msg.view_format);
         }
         
-        // if this msg.topic has no charts, create a array for them
+        // if this msg.topic has no charts, create a array for it
         try {
              if (vpn_data[msg.topic].charts == undefined) {
                 vpn_data[msg.topic].charts = [];
@@ -88,27 +104,25 @@
         }
 
         
-        for (var emit_config in config) {
+        // iterate over objects in the view_format
+        for (var emit_config in view_format) {
             
-            var metric_list = config[emit_config].show;
+            var metric_list = view_format[emit_config].show;
             
             if (vpn_data[msg.topic].charts[emit_config] == undefined) {
 
                 // always log creations
                 console.log("create chart config: " + emit_config);
-                console.log(config[emit_config]);
+                console.log(view_format[emit_config]);
                 
 
-                var div_name = (config[emit_config].div === undefined) ? "bigcharts" : config[emit_config].div;
-                var chart_type = (config[emit_config].chart_type == undefined) ? "stackedColumn" : config[emit_config].chart_type;
-                var chart_length = (config[emit_config].chart_length == undefined) ? 10 : config[emit_config].chart_length;
-                var counter = (config[emit_config].counter == undefined) ? false : config[emit_config].counter;
-                var metric_list = config[emit_config].show;
-                var parent_is_array = (config[emit_config].parent_is_array == undefined) ? false : config[emit_config].parent_is_array;
-                
-                // if parent_path_is_array, you might want to set name of the metric in config.
-                // var metric_key_name = (config[emit_config].metric_key_name == undefined) ? "name" : config[emit_config].metric_key_name;
-                
+                var div_name = (view_format[emit_config].div === undefined) ? "bigcharts" : view_format[emit_config].div;
+                var chart_type = (view_format[emit_config].chart_type == undefined) ? "stackedColumn" : view_format[emit_config].chart_type;
+                var chart_length = (view_format[emit_config].chart_length == undefined) ? 10 : view_format[emit_config].chart_length;
+                var counter = (view_format[emit_config].counter == undefined) ? false : view_format[emit_config].counter;
+                var metric_list = view_format[emit_config].show;
+                var parent_is_array = (view_format[emit_config].parent_is_array == undefined) ? false : view_format[emit_config].parent_is_array;
+                                
                 vpn_data[msg.topic].charts[emit_config] = new ZChart(emit_config, chart_length, div_name, chart_type);
                 vpn_data[msg.topic].charts[emit_config].counter = counter;
                 
@@ -122,22 +136,15 @@
                         {
                             type: chart_type,
                             dataPoints: [],
-                            name: config[emit_config].show[e],
+                            name: view_format[emit_config].show[e],
                             showInLegend: true,
                         }
                     );
                 }
             }
 
-            var data_path = config[emit_config].data_path;
+            var data_path = view_format[emit_config].data_path;
 
-            // if (data_path != undefined) {
-            //     console.log("requesting sub shift");
-            //     console.log(data);
-            //     data = data_shifter(data, data_path, true);
-            // }
-
-            
             for (var e in metric_list ){
 
                 if (logging) {
@@ -147,8 +154,8 @@
                 var tmp_results = 0;
                 
                 // ADD_TOGETHER if you really want it... gonna remove this I think.
-                if ( config[emit_config].modifier != undefined ) {
-                    if ( config[emit_config].modifier == "add_together" ) {
+                if ( view_format[emit_config].modifier != undefined ) {
+                    if ( view_format[emit_config].modifier == "add_together" ) {
                         try {
                             tmp_results = tmp_results + data[metric_list[e]];
                         } catch (err) {
@@ -178,49 +185,10 @@
                             console.log(data);
                             console.log("---END---");
                         }
-                        
-                    // } else if (parent_is_array) {
-                    //     console.log("array instance by config");
-                    //     try {
-                    //         for (var item in data) {
-                    //             console.log("data: ");
-                    //             console.log(data[item]);
-
-                    //             var tmp_data = data[item];
-
-                    //             try {
-                    //                 var path_hint = data_path.split(".");
-                    //                 for (var word in path_hint) {
-                    //                     data = data[path_hint[word]];
-                    //                 }
-
-                    //             } catch (err) {
-                    //                 app_error("Error in data_path: " + msg.topic);
-                    //                 console.log("error in data_path: " + msg.topic);
-                    //                 console.log(msg);
-                    //             }
-
-                    //             tmp_results = tmp_data.data[item][metric_list[e]];
-
-                                
-                    //             console.log("tmp_results: " + tmp_results);
-                    //             plot_data(vpn_data[msg.topic].charts[emit_config], e, tmp_results, tmpDate);
-                    //         }
-                    //     } catch (err) {
-                    //         app_error("Not a array");
-                    //     }
 
                     } else {
                         try {
-
-
                             data = data_shifter(data, data_path, logging);
-
-                            // var path_hint = data_path.split(".");
-                            // for (var word in path_hint) {
-                            //     data = data[path_hint[word]];
-                            // }
-
                         } catch (err) {
                             app_error("Data Path Error: " + msg.topic + "/" + metric_list[e]);
                             console.log("error in data_path: " + msg.topic);
