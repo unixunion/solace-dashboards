59a982dda454a4cd1edf9d18e9b92dd40ba2fc71
Kegan Holtzhausen <Kegan.Holtzhausen@unibet.com>
2015-06-09 12:45:24 +0200

    1.2.1

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 JAVA_OPTS="-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory"
 JAVA_HOME="/opt/jdk1.8.0_45"
-$JAVA_HOME/bin/java $JAVA_OPTS -jar solace-dashboards-1.2.0-fat.jar -conf conf.json
+$JAVA_HOME/bin/java $JAVA_OPTS -jar solace-dashboards-1.2.1-fat.jar -conf conf.json
