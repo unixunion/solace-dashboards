adccb6a77166391d8a98fb351c9518896f910441
Kegan Holtzhausen <Kegan.Holtzhausen@unibet.com>
2015-06-14 23:10:54 +0200

    cleanup Server and formatting

--- src/main/java/com/deblox/server/Server.java
+++ src/main/java/com/deblox/server/Server.java
@@ -22,7 +22,6 @@ limitations under the License.
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
@@ -36,7 +35,6 @@ public class Server extends AbstractVerticle {
 
   private static final Logger logger = LoggerFactory.getLogger(Server.class);
   EventBus eb;
-  JsonObject config;
 
   @Override
   public void start(Future<Void> startFuture) {
@@ -54,10 +52,15 @@ public class Server extends AbstractVerticle {
     router.route("/eventbus/*").handler(ebHandler);
 
     // Serve the static pages
-    router.route().handler(StaticHandler.create());
+    router.route().handler(StaticHandler.create()
+            .setCachingEnabled(false)
+            .setWebRoot(config().getString("webroot", "webroot"))
+            .setDirectoryListing(false));
 
     // the server itself
-    vertx.createHttpServer().requestHandler(router::accept).listen(config().getInteger("port", 8080));
+    vertx.createHttpServer()
+            .requestHandler(router::accept)
+            .listen(config().getInteger("port", 8080));
 
     // need a bus!
     eb = vertx.eventBus();
